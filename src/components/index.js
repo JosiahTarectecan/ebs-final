80f0f39a2cf4596d07e79eb8d4b75c04c9081b2c
Josiah Tarectecan <97994495+manemgeb@users.noreply.github.com>
2023-01-24 16:42:54 -0700

    commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export {default as Navbar} from './NavBar/Navbar';
 export {default as Products} from './Products/Products';
 export {default as Cart} from './Cart/Cart';
-export {default as DropMenu} from './Menu/DropMenu';
