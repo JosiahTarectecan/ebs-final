bfc205e7996e1fc3e733a495b8d103fbf120e243
Arjun Ramanan <Arjun.Ramanan@edu.sait.ca>
2023-02-11 00:03:05 -0700

    Payment

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { commerce } from './lib/Commerce';
-import {Products, Navbar, Cart, Checkout} from './components';
+import {Products, Navbar, Cart, Checkout, About, Home} from './components';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { set } from 'react-hook-form';
 
 const App = () => {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState({});
+  const [about] = useState({});
+  const [home] = useState({});
+  const [order, setOrder] = useState({});
+  const [errorMessage, setErrorMessage] = useState('');
+
+
 
   const fetchProducts = async () => {
     const {data} = await commerce.products.list();
@@ -34,6 +41,21 @@ const App = () => {
     setCart(item);
     };
 
+    const refreshCart = async () => {
+      const newCart = await commerce.cart.refresh();
+      setCart(newCart);
+    }
+
+    const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+      try {
+        const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder);
+        setOrder(incomingOrder);
+        refreshCart();
+      } catch (error) {
+        setErrorMessage(error.data.error.message)
+      }
+    }
+
 
   useEffect(() => {
 fetchProducts();
@@ -50,7 +72,12 @@ fetchCart();
           <Routes>
           <Route path='/' element={<Products products = {products} onAddToCart = {handleAddToCart}/>} /> 
           <Route path='/cart' element={<Cart cart={cart} handleUpdateCartQty={handleUpdateCartQty} handleRemoveFromCart={handleRemoveFromCart} handleEmptyCart={handleEmptyCart} />} />
-         <Route path="/checkout" element={<Checkout cart={cart} />} />
+          <Route path="/checkout" element={
+          <Checkout cart={cart} order={order} onCaptureCheckout={handleCaptureCheckout} error={errorMessage}/>} />
+          <Route path="/about" element={<About about={about}/>} />
+          <Route path="/home" element={<Home home={home}/>} />
+
+
         </Routes>
       </div>
     </Router>
